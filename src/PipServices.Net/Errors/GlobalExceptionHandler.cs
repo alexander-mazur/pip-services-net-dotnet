61e017316b6e6d116d8f5e8496a2630c6c3dd48b
vovant <vovantigo@gmail.com>
2016-11-03 14:57:15 +0200

    Global exception handler class for .Net Core API was added

---
+++ src/PipServices.Net/Errors/GlobalExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PipServices.Commons.Errors;
+
+namespace PipServices.Net.Errors
+{
+    public class GlobalExceptionHandler : IExceptionFilter, IDisposable
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var correlationId = context.HttpContext.Request.Query["correlation_id"];
+
+            var response = ErrorDescriptionFactory.Create(context.Exception);
+            response.CorrelationId = correlationId;
+
+            context.Result = new JsonResult(response)
+            {
+                StatusCode = response.Status
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
