3ad70fc9f95b9514a0986fd5fcb6df34153b881b
vovant <vovantigo@gmail.com>
2016-12-06 20:06:17 +0200

    Entry point code was added

--- examples/PipServices.Dummy.Runner.Net45/Program.cs
+++ examples/PipServices.Dummy.Runner.Net45/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using PipServices.Commons.Config;
+using PipServices.Commons.Refer;
+using PipServices.Dummy.Runner.Persistance;
+using PipServices.Dummy.Runner.Services;
 
-namespace PipServices.Dummy.Runner.Net45
+namespace PipServices.Dummy.Runner
 {
     class Program
     {
+        private static readonly ManualResetEvent _exitEvent = new ManualResetEvent(false);
+
+        private static readonly ConfigParams RestConfig = ConfigParams.FromTuples(
+            "connection.protocol", "http",
+            "connection.host", "localhost",
+            "connection.port", 3000
+            );
+
         static void Main(string[] args)
         {
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                _exitEvent.Set();
+
+                Environment.Exit(1);
+            };
+
+            var ctrl = new DummyRepository();
+
+            var service = new DummyRestService();
+            service.Configure(RestConfig);
+
+            var references = References.FromList(ctrl, service);
+
+            service.SetReferences(references);
+
+            var task = service.OpenAsync(null);
+            task.Wait();
+
+            // Wait and close
+            _exitEvent.WaitOne();
         }
     }
 }
