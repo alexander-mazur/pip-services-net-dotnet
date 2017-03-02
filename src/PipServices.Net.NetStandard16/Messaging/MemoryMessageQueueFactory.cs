e16170c67e3a092e273c94b62dc8f90f3059fbcf
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-01 17:17:53 -0800

    'Migrated to pip-services 2.0.3'

--- src/PipServices.Net.NetStandard16/Messaging/MemoryMessageQueueFactory.cs
+++ src/PipServices.Net.NetStandard16/Messaging/MemoryMessageQueueFactory.cs
@@ -1,39 +1,19 @@
 ﻿using PipServices.Commons.Build;
 using PipServices.Commons.Refer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PipServices.Net.Messaging
 {
-    public class MemoryMessageQueueFactory : IFactory
+    public class MemoryMessageQueueFactory : Factory
     {
         public static readonly Descriptor Descriptor = new Descriptor("pip-services-net", "factory", "message-queue", "memory", "1.0");
-        public static readonly Descriptor QueueDescriptor = new Descriptor("*", "message-queue", "memory", "*", "*");
+        public static readonly Descriptor MemoryQueueDescriptor = new Descriptor("pip-services-net", "message-queue", "memory", "*", "*");
 
-        public MemoryMessageQueueFactory() { }
-
-        public Descriptor GetDescriptor()
+        public MemoryMessageQueueFactory()
         {
-            return Descriptor;
-        }
-
-        public bool CanCreate(object locator)
-        {
-            var descriptor = locator as Descriptor;
-            if (descriptor == null || !descriptor.Match(QueueDescriptor))
-                return false;
-            return true;
-        }
-
-        public object Create(object locator)
-        {
-            var descriptor = locator as Descriptor;
-            if (descriptor == null || !descriptor.Match(QueueDescriptor))
-                throw new CreateException(null, locator);
-
-            return new MemoryMessageQueue(descriptor.Name);
+            Register(MemoryQueueDescriptor, (locator) => {
+                Descriptor descritor = (Descriptor)locator;
+                return new MemoryMessageQueue(descritor.Name);
+            });
         }
     }
 }
